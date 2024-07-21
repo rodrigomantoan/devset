d34d0e8d61a4da7351bdc9b64a15a2ca5ba8523c
rodrigomantoan <rodrigomantoan@gmail.com>
2024-07-21 19:56:17 -0300

    Add new commands to help

--- commands/help.sh
+++ commands/help.sh
@@ -11,4 +11,6 @@ help() {
 
   _print_message "\n\033[1;34mAvailable commands:\033[0m"
   _print_message "${GREEN}  help                ${DEFAULT}Display help message"
+  _print_message "${GREEN}  version             ${DEFAULT}Display application version"
+  _print_message "${GREEN}  new, add, create    ${DEFAULT}Create a new project (automated process for Laravel, WordPress, and Statamic)"
 }
